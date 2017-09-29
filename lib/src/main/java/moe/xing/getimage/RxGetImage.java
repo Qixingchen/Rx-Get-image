725aaed6a4ce33404bae9d4e6b23aeb91957da40
qixingchen <qixingchen@lovelive.moe>
2017-09-29 12:00:12 +0800

    remove Deprecated method : getInstance

--- lib/src/main/java/moe/xing/getimage/RxGetImage.java
+++ lib/src/main/java/moe/xing/getimage/RxGetImage.java
@@ -20,25 +20,10 @@ import rx.Subscriber;
 @SuppressWarnings("WeakerAccess")
 public class RxGetImage {
 
-    private static RxGetImage sSingleton;
     private SparseArray<Subscriber<? super File>> mSubscribers = new SparseArray<>();
 
     public RxGetImage() {
 
-    }
-
-    /**
-     * 获取单例
-     */
-    public static RxGetImage getInstance() {
-        if (sSingleton == null) {
-            synchronized (RxGetImage.class) {
-                if (sSingleton == null) {
-                    sSingleton = new RxGetImage();
-                }
-            }
-        }
-        return sSingleton;
     }
 
     public static Builder newBuilder() {
